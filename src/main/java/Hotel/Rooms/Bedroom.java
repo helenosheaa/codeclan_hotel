eb6221a2e6cdc11c4f1e8acafdd015cfc244d5f7
helenosheaa <helenosheaa@gmail.com>
2018-06-29 12:07:38 +0100

    bedroom get capacity, guests, room number

--- src/main/java/Hotel/Rooms/Bedroom.java
+++ src/main/java/Hotel/Rooms/Bedroom.java
@@ -1,4 +1,26 @@
 package Hotel.Rooms;
 
-public class Bedroom {
+import Hotel.Guest;
+
+import java.util.ArrayList;
+
+public class Bedroom extends Room {
+
+    private String type;
+    private int roomNumber;
+    private double rate;
+
+    public Bedroom(int capacity, ArrayList<Guest> guests, String type, int roomNumber, double rate) {
+        super(capacity, guests);
+        this.type = type;
+        this.roomNumber = roomNumber;
+        this.rate = rate;
+    }
+
+
+    public int getRoomNumber() {
+        return this.roomNumber;
+    }
 }
+
+
--- src/test/java/Hotel/Rooms/BedroomTest.java
+++ src/test/java/Hotel/Rooms/BedroomTest.java
@@ -1,4 +1,42 @@
 package Hotel.Rooms;
 
+import Hotel.Guest;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    Bedroom bedroom;
+    ArrayList<Guest> guests;
+    Guest guest1;
+    Guest guest2;
+
+    @Before
+    public void before(){
+        guest1 = new Guest("Nick");
+        guest2 = new Guest("Helen");
+        guests = new ArrayList<>();
+        guests.add(guest1);
+        guests.add(guest2);
+        bedroom = new Bedroom(2, guests, "double", 21, 78.99);
+    }
+
+    @Test
+    public void canGetCapacity(){
+        assertEquals(2, bedroom.getCapacity());
+    }
+
+    @Test
+    public void canGetGuests(){
+        assertEquals(guests, bedroom.getGuests());
+    }
+
+    @Test
+    public void canGetRoomNumber(){
+        assertEquals(21,bedroom.getRoomNumber());
+    }
 }
