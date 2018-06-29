00b4e6a9afaca17bb99a106ad264cdaf49eb3f05
helenosheaa <helenosheaa@gmail.com>
2018-06-29 14:45:15 +0100

    guest has name

--- src/test/java/Hotel/GuestTest.java
+++ src/test/java/Hotel/GuestTest.java
@@ -1,4 +1,21 @@
 package Hotel;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("Jim");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Jim", guest.getName())
+    }
 }
